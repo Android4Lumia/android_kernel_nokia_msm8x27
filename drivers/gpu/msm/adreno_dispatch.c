7992db05821ee67eac7c02da37de1d3252832041
Prabhat Awasthi <pawasthi@codeaurora.org>
2014-01-02 17:31:29 +0530

    msm: kgsl: Increase fault timer for A305B GPU
    Slower GPU's need larger fault timer to detect hangs
    correctly. Frodo A305B GPU is slower compared to faster
    GPU, hence use a larger 200ms fault timer to prevent
    false hang detection.
    
    Signed-off-by: Prabhat Awasthi <pawasthi@codeaurora.org>
    Change-Id: I6338cecc3be81700ab10c45fab66daa7a1078e42

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -1583,8 +1583,8 @@ void adreno_dispatcher_start(struct kgsl_device *device)
 
 	complete_all(&device->cmdbatch_gate);
 
-	/* a305c GPU is slower than a330 and needs a larger timer */
-	if (adreno_is_a305c(adreno_dev))
+	/* a305b & a305c GPUs are slower than a330 and needs a larger timer */
+	if (adreno_is_a305b(adreno_dev) || adreno_is_a305c(adreno_dev))
 		_fault_timer_interval = 200;
 
 	/* Schedule the work loop to get things going */
