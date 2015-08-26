99accf113dfcac951a224d612cbd312015d52330
Terence Ho <terenceh@codeaurora.org>
2015-08-26 17:02:24 -0400

    msm: camera: Fix potential null pointer dereference
    Add check to avoid potential null pointer
    dereference.
    
    Change-Id: Ie27ed5f32ffc5ff73ee72018e53854003112ca24
    Signed-off-by: Terence Ho <terenceh@codeaurora.org>

--- drivers/media/video/msm/server/msm_cam_server.c
+++ drivers/media/video/msm/server/msm_cam_server.c
@@ -1069,6 +1069,12 @@ int msm_server_v4l2_subscribe_event(struct v4l2_fh *fh,
 {
 	int rc = 0;
 
+	if (!fh || !sub) {
+		pr_err("%s: NULL pointer fh 0x%p sub 0x%p",
+			__func__, fh, sub);
+		return -EINVAL;
+	}
+
 	D("%s: fh = 0x%x, type = 0x%x", __func__, (u32)fh, sub->type);
 	if (sub->type == V4L2_EVENT_ALL) {
 		/*sub->type = MSM_ISP_EVENT_START;*/
@@ -1107,6 +1113,12 @@ int msm_server_v4l2_unsubscribe_event(struct v4l2_fh *fh,
 {
 	int rc = 0;
 	struct v4l2_event ev;
+
+	if (!fh || !sub) {
+		pr_err("%s: NULL pointer fh 0x%p sub 0x%p",
+			__func__, fh, sub);
+		return -EINVAL;
+	}
 
 	D("%s: fh = 0x%x\n", __func__, (u32)fh);
 
