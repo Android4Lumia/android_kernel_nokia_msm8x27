322db836ce0b754144d35517bd90fbc11132769f
RonGokhale <cips173@gmail.com>
2015-12-25 13:03:28 +0530

    Revert "radio: iris: Fix the possibility of NULL pointer access"
    This reverts commit 79abcd96f67dc16ad5b07ea99a70dd3e74905f98.

--- drivers/media/radio/radio-iris.c
+++ drivers/media/radio/radio-iris.c
@@ -4872,8 +4872,7 @@ static int iris_fops_release(struct file *file)
 		return retval;
 	}
 END:
-	if (radio->fm_hdev != NULL)
-		radio->fm_hdev->close_smd();
+	radio->fm_hdev->close_smd();
 	if (retval < 0)
 		FMDERR("Err on disable FM %d\n", retval);
 
