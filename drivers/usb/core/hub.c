57ee778a2e35e676d79017d239f258f725e8ef50
Chandana Kishori Chiluveru <cchiluve@codeaurora.org>
2016-05-17 11:43:54 +0530

    USB: core: Add USB_DEVICE_ERROR uevent for enumeration timeout
    Some userspace modules expect USB driver to send event to userspace, when
    usb device enumeration failure with device descriptor read errors.
    Add USB_DEVICE_ERROR uevent in the USB driver to explicitly send the ERROR
    uevent for such case.
    
    Change-Id: I7128869ff0700ab90d4c949de24cd03c5c90e22e
    Signed-off-by: Chandana Kishori Chiluveru <cchiluve@codeaurora.org>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -3059,6 +3059,7 @@ hub_port_init (struct usb_hub *hub, struct usb_device *udev, int port1,
 	enum usb_device_speed	oldspeed = udev->speed;
 	const char		*speed;
 	int			devnum = udev->devnum;
+	char			*error_event[] = { "USB_DEVICE_ERROR=Device_No_Response", NULL };
 
 	/* root hub ports have a slightly longer reset period
 	 * (from USB 2.0 spec, section 7.1.7.5)
@@ -3222,6 +3223,8 @@ hub_port_init (struct usb_hub *hub, struct usb_device *udev, int port1,
 				dev_err(&udev->dev,
 					"device descriptor read/64, error %d\n",
 					r);
+				kobject_uevent_env(&udev->dev.kobj,
+						KOBJ_CHANGE, error_event);
 				retval = -EMSGSIZE;
 				continue;
 			}
@@ -3270,6 +3273,8 @@ hub_port_init (struct usb_hub *hub, struct usb_device *udev, int port1,
 			dev_err(&udev->dev,
 					"device descriptor read/8, error %d\n",
 					retval);
+			kobject_uevent_env(&udev->dev.kobj,
+						KOBJ_CHANGE, error_event);
 			if (retval >= 0)
 				retval = -EMSGSIZE;
 		} else {
